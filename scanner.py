2314bf6d98c0b6f6ee9a8c91de50dc1aa6e60783
Patryk Kocioł <pat.kociol@gmail.com>
2024-02-25 19:26:02 +0100

    add config reading from txt file

--- scanner.py
+++ scanner.py
@@ -10,11 +10,11 @@ config= os.path.join(cd, "config.txt")
 log = os.path.join(cd, "log.txt")
 icon = os.path.join(cd, "icon.ico") if platform.system() == 'Windows' else None
 
-
 host_ip = []
 ports_opened = []
 scan_interval = 90 
 
+#Notifying unauthorized open ports using system notifications & log.txt file
 def warning(ports,host):
     
     with open(log, 'a') as file:
@@ -27,3 +27,32 @@ def warning(ports,host):
         app_icon=icon ,  
         timeout=10, 
     )
+    
+def read_conf():
+    
+    temp = []
+    with open(config, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.replace('\n','')
+            if line.startswith("#") or not line:
+                continue
+            elif line[0].isdigit():
+                temp = line.split(' ')
+                scan_interval = temp[0]
+                temp = []
+            elif line.startswith('port-range'):
+                temp = line.split(' ')
+                p_range_first = temp[1]
+                p_range_last = temp[2]
+                temp = []
+            elif line.startswith('Host'):
+                temp = line.split(' ')
+                host_ip.append(temp[1])
+                del temp[0:2]
+                ports_opened.append(temp)
+                temp = []
+            else:
+                continue
+    file.close 
+    return scan_interval, p_range_first, p_range_last, host_ip, ports_opened
